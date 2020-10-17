74efc82a5a4b6384f85865b19ad2eaedb20a600c
lektor925 <partizan925@mail.ru>
2020-10-17 17:33:31 +0300

    Day 2. Решена задача с инопланетянами.

--- 3_6_step_3.py
+++ 3_6_step_3.py
@@ -1,10 +1,14 @@
 import time
 import math
+from selenium.webdriver.support import expected_conditions as EC
+
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
@@ -13,19 +17,29 @@ def browser():
     browser.quit()
 
 
-# @pytest.mark.parametrize("step", ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
-@pytest.mark.parametrize("step", ["236895", "236896"])
+@pytest.mark.parametrize("step", ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
+# @pytest.mark.parametrize("step", ["236895", "236896"])
 def test_my_work(browser, step):
     link = f"https://stepik.org/lesson/{step}/step/1"
     browser.get(link)
-    browser.implicitly_wait(75)
 
-    my_textarea = browser.find_element_by_css_selector("textarea")
-    answer = math.log(int(time.time()))
+    # my_textarea = browser.find_element_by_css_selector("textarea[data-autogrow]")
+    ta_css = 'textarea[data-autogrow]'
+    my_textarea = WebDriverWait(browser, 25).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, ta_css))
+    )
+    # answer = int(math.log(int(time.time())))
+    answer = str(math.log(int(time.time()-2)))
+    # print(answer)
     my_textarea.send_keys(answer)
-    button = browser.find_element_by_css_selector(".submit-submission")
+    button = WebDriverWait(browser, 25).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "submit-submission"))
+    )
     button.click()
 
-    msg_text = browser.find_element_by_css_selector(".smart-hints__hint").text
+    msg_el = WebDriverWait(browser, 25).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, "smart-hints__hint"))
+    )
+    msg_text = msg_el.text
     print(msg_text)
     assert msg_text == 'Correct!', 'Неверный ответ'
